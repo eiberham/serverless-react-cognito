47eb8fde4a320c4d3178402c846353760b77fd9f
Abraham <cedenoabraham@gmail.com>
2020-10-22 00:35:15 -0300

    protected hoc/hook

--- src/Protected.js
+++ src/Protected.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from 'react';
-import { Auth } from 'aws-amplify'
+import React from 'react';
 import Container from './Container'
 
+import protectedRoute from './protectedRoute'
+
 function Protected(props) {
-  useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .catch(() => {
-        props.history.push('/profile')
-      })
-  }, [])
   return (
     <Container>
       <h1>Protected route</h1>
@@ -16,4 +11,4 @@ function Protected(props) {
   );
 }
 
-export default Protected
+export default protectedRoute(Protected)
