5636a0745c9befa8968691c66e21a0c8be216cf6
Abraham <cedenoabraham@gmail.com>
2020-10-24 23:44:55 -0300

    form components files

---
+++ src/ForgotPasswordSubmit.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Button = ({title, onClick}) => {
+    return <button style={styles.button} onClick={onClick}>{title}</button>
+};
+
+const styles = {
+    button: {
+        backgroundColor: '#006bfc',
+        color: 'white',
+        width: 316,
+        height: 45,
+        fontWeight: '600',
+        fontSize: 14,
+        cursor: 'pointer',
+        border: 'none',
+        outline: 'none',
+        borderRadius: 3,
+        marginTop: '25px',
+        boxShadow: '0px 1px 3px rgba(0,0,0,.3)',
+
+    },
+};
+
+export default Button;
---
+++ src/Form.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Auth } from 'aws-amplify';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import ConfirmSignUp from './ConfirmSignUp';
+import ForgotPassword from './ForgotPassword';
+import ForgotPasswordSubmit from './ForgotPasswordSubmit';
+
+const initialFormState = { username: '', password: '', email: '', confirmationCode: ''};
+
+function Form(props) {
+    const [ formType, updateFormType ] = useState('signIn');
+    const [ formState, updateFormState ] = useState(initialFormState);
+
+    function renderForm() {}
+
+    return (
+        <div>{renderForm()}</div>
+    )
+}
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        height: 45,
+        marginTop: 8,
+        width: 300,
+        maxWidth: 300,
+        padding: '0px 8px',
+        fontSize: 16,
+        outline: 'none',
+        border: 'none',
+        borderBotton: '2px solid rgba(0, 0, 0, .3)'
+    },
+    toggleForm: {
+        fontWeight: 600,
+        padding: '0px 25px',
+        marginTop: '15px',
+        marginBottom: '0px',
+        textAlign: 'center',
+        color: 'rgba(0, 0, 0, .6)',
+    },
+    resetPassword: {
+        marginTop: '5px',
+    },
+    anchor: {
+        color: '0006bfc',
+        cursor: 'pointer',
+    },
+};
+
+export { styles, Form as default };
+
+export default Form;
